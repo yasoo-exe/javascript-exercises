d65e6ae375baeca34296aaac0b9ae55d0f03522c
yasoo-exe <76653084+yasoo-exe@users.noreply.github.com>
2023-09-24 14:40:41 +0500

    solved 4th exercise, all tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...removeArgs) {
+  let result = [];
+  arr.forEach((x) => (!removeArgs.includes(x) ? result.push(x) : x));
+  return result;
 };
 
 // Do not edit below this line
