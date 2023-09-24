ca15c3061f5c52a40742bf4d8f26e60299322971
yasoo-exe <76653084+yasoo-exe@users.noreply.github.com>
2023-09-24 15:02:15 +0500

    finished exercise, passing all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (first, second) {
+  if (
+    first >= 0 &&
+    second >= 0 &&
+    Number.isInteger(first) &&
+    Number.isInteger(second)
+  ) {
+    let tempArr = [first, second].sort((a, b) => a - b);
+    console.log(tempArr);
+    let result = 0;
+    for (let i = tempArr[0]; i <= tempArr[1]; i++) {
+      result += i;
+    }
+    return result;
+  }
+  return "ERROR";
 };
+
+console.log(sumAll("s", 4));
 
 // Do not edit below this line
 module.exports = sumAll;
