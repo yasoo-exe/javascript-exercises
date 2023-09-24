8f7910a6a423c8b1a1e9c101b02ccb5534c7b3f4
yasoo-exe <76653084+yasoo-exe@users.noreply.github.com>
2023-09-24 15:19:58 +0500

    solved with all passing tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 100 === 0 && year % 400 === 0 && year % 4 === 0) {
+    return true;
+  }
+  if (year % 100 === 0 && year % 400 !== 0 && year % 4 === 0) {
+    return false;
+  }
+  if (year % 100 !== 0 && year % 4 === 0) {
+    return true;
+  }
+  return false;
 };
+
+console.log(leapYears(1997));
 
 // Do not edit below this line
 module.exports = leapYears;
