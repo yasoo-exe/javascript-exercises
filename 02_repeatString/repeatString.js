e0ea282ee98bcf4b61a0e083ffaca5bbfb308aca
yasoo-exe <76653084+yasoo-exe@users.noreply.github.com>
2023-09-24 14:23:03 +0500

    2nd task completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  let repeated = "";
+  if (num >= 0) {
+    for (let i = 0; i < num; i++) {
+      repeated += str;
+    }
+    return repeated;
+  }
+  return "ERROR";
 };
+
+console.log(repeatString("cat", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
