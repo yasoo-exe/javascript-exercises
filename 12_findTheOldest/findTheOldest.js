5c0a05d0c0cf5c954579b38fef2aeb615dc30885
yasoo-exe <76653084+yasoo-exe@users.noreply.github.com>
2023-10-01 23:47:51 +0500

    finished ex 12 with all tests passing

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,36 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (!arr[i].yearOfDeath) {
+      arr[i].yearOfDeath = 2023;
+    }
+  }
+  let result = [];
+  arr.map((x) => {
+    return result.push({ name: x.name, age: x.yearOfDeath - x.yearOfBirth });
+  });
+  return result.sort((a, b) => {
+    return a.age < b.age ? 1 : -1;
+  })[0];
 };
+
+console.log(
+  findTheOldest([
+    {
+      name: "Carly",
+      yearOfBirth: 1066,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ])
+);
 
 // Do not edit below this line
 module.exports = findTheOldest;
