0f235884a347601703946837edcdb2e6caf5c389
yasoo-exe <76653084+yasoo-exe@users.noreply.github.com>
2023-10-01 23:10:42 +0500

    solved ex-9 with all tests passing

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (arg) {
+  let temp = arg.match(/[\d\w]/gi);
+  return temp.join("").toLowerCase() === temp.reverse().join("").toLowerCase();
 };
 
 // Do not edit below this line
