4135354dd386bc69bbf661a75a6fac77f540fa65
yasoo-exe <76653084+yasoo-exe@users.noreply.github.com>
2023-10-01 23:20:17 +0500

    finished ex 11 with all tests passing

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (obj) {
+  let result = [];
+  obj.forEach((element) => {
+    result.push(element.title);
+  });
+  return result;
 };
+
+const books = [
+  {
+    title: "Book",
+    author: "Name",
+  },
+  {
+    title: "Book2",
+    author: "Name2",
+  },
+];
+
+console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
