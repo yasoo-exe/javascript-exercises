670751a1db4087163d2c4022dbd0c43e916c8a33
yasoo-exe <76653084+yasoo-exe@users.noreply.github.com>
2023-10-02 09:09:29 +0500

    ex 10 solved with all tests passing, repo solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (n < 0) {
+    return "OOPS";
+  }
+  var i;
+  var fib = [1, 1]; // Initialize array!
 
+  for (i = 2; i <= n - 1; i++) {
+    // Next fibonacci number = previous + one before previous
+    // Translated to JavaScript:
+    fib[i] = fib[i - 2] + fib[i - 1];
+  }
+  return fib[fib.length - 1];
 };
 
+//In this exercise, the Fibonacci sequence used is 1, 1, 2, 3, 5, 8, etc.
+
+console.log(fibonacci(4));
 // Do not edit below this line
 module.exports = fibonacci;
